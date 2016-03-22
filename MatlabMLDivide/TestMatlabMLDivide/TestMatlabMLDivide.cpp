2b5c987b532f6b396169ddf4436e93af818e93c1
BakaNeko <meganekko@list.ru>
2016-03-22 19:08:23 +0300

    Added MSVC project.

---
+++ MatlabMLDivide/TestMatlabMLDivide/TestMatlabMLDivide.cpp
@@ -0,0 +1,15 @@
+// TestMatlabMLDivide.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hell yeah!" << endl;
+	return 0;
+}
+
