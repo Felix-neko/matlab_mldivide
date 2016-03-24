365f256b3dc3d7356bf76934aace32b6feb056b4
BakaNeko <meganekko@list.ru>
2016-03-24 13:09:53 +0300

    Minor renaming...

--- py_matlab_matrix_divide/setup.py
+++ py_matlab_matrix_divide/setup.py
@@ -16,10 +16,10 @@ my_sources = filter(lambda x: x.endswith(".cpp") or x.endswith(".c"), os.listdir
 my_sources = [os.path.join(my_source_dir, source) for source in my_sources]
 
 
-setup(name='PyMatlabDivide',
-      version='0.0',
+setup(name='PyMatlabMatrixDivide',
+      version='0.1',
+      author="Woody W. Woodpecker",
       ext_modules=[Extension("matlab_matrix_divide._matlab_matrix_divide", matlab_sources + my_sources,
                              include_dirs=[matlab_source_dir, my_source_dir])],
-
       packages=['matlab_matrix_divide']
       )
