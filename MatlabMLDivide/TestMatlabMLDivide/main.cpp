d6f2a3a87b20f42719ecd038b92f412398fa7332
BakaNeko <meganekko@list.ru>
2016-12-28 13:06:06 +0300

    © Krepak

--- MatlabMLDivide/TestMatlabMLDivide/main.cpp
+++ MatlabMLDivide/TestMatlabMLDivide/main.cpp
@@ -1,7 +1,7 @@
 // TestMatlabMLDivide.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 
 #include <iostream>
 using namespace std;
--- py_matlab_matrix_divide/setup.py
+++ py_matlab_matrix_divide/setup.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 from distutils.core import setup, Extension
 
-import os
 
 matlab_source_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                     "../codegen/lib/matlab_mldivide"))
