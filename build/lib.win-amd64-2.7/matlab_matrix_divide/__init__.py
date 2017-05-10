356ec6eda631c192ddcc4c13cad46f96075bf32c
Felix-Neko <Felix-Neko@list.ru>
2017-05-10 17:37:36 +0300

    Added a test for nonsquare matrices (FAILED).

--- build/lib.win-amd64-2.7/matlab_matrix_divide/__init__.py
+++ build/lib.win-amd64-2.7/matlab_matrix_divide/__init__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+import glob
 from os.path import abspath, dirname, join
 import ctypes
 
-library_path = join(dirname(abspath(__file__)), "_matlab_matrix_divide.pyd")
 
-if not os.path.exists(library_path):
-    library_path = join(abspath(os.path.join(dirname(os.path.abspath(__file__)),
-                        "../build/lib.win-amd64-2.7/matlab_matrix_divide/_matlab_matrix_divide.pyd")))
+def library_path():
+    ext = '.so' if os.name == 'posix' else '.pyd'
+    dir = join(dirname(abspath(__file__)))
+    print(dir)
+    matches = glob.glob(join(dir, '_matlab_matrix_divide*%s' % ext))
+    return matches[0]
 
-library = ctypes.CDLL(library_path)
+
+library = ctypes.CDLL(library_path())
--- matlab_matrix_divide/__init__.py
+++ matlab_matrix_divide/__init__.py
@@ -9,6 +9,7 @@ import ctypes
 def library_path():
     ext = '.so' if os.name == 'posix' else '.pyd'
     dir = join(dirname(abspath(__file__)))
+    print(dir)
     matches = glob.glob(join(dir, '_matlab_matrix_divide*%s' % ext))
     return matches[0]
 
