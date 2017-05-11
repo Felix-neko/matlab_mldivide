3adb136005bd34f99ac25be1af883e7551fb6ea7
Felix-Neko <Felix-Neko@list.ru>
2017-05-11 17:29:14 +0300

    Added order conversion...

--- matlab_matrix_divide/matrix_divide.py
+++ matlab_matrix_divide/matrix_divide.py
@@ -19,18 +19,18 @@ def mldivide(a_matrix, b_matrix):
         :class:`numpy.ndarray(dtype=numpy.complex128)`: an ``N``-by-``P`` array
     """
 
-    a_matrix = np.asarray(a_matrix, dtype=np.complex128)
+    a_matrix = np.asarray(a_matrix, dtype=np.complex128, order="F")
     if len(a_matrix.shape) != 2:
         raise ValueError("a_matrix should be an 2D array!")
 
-    b_matrix = np.asarray(b_matrix, dtype=np.complex128)
+    b_matrix = np.asarray(b_matrix, dtype=np.complex128, order="F")
     if len(b_matrix.shape) != 2:
         raise ValueError("b_matrix should be a 2D array!")
 
     if a_matrix.shape[0] != b_matrix.shape[0]:
         raise ValueError("a_matrix should have the same number of rows as b_matrix!")
 
-    c_matrix = np.zeros(shape=(a_matrix.shape[1], b_matrix.shape[1]), dtype=np.complex128)
+    c_matrix = np.zeros(shape=(a_matrix.shape[1], b_matrix.shape[1]), dtype=np.complex128, order="F")
 
     a_ptr = a_matrix.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
     b_ptr = b_matrix.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
@@ -39,4 +39,4 @@ def mldivide(a_matrix, b_matrix):
     library.mldivide(a_ptr, a_matrix.shape[0], a_matrix.shape[1], b_ptr, b_matrix.shape[0], b_matrix.shape[1],
                      c_ptr, c_matrix.shape[0], c_matrix.shape[1])
 
-    return c_matrix
+    return np.asarray(c_matrix, order="C")
--- test.py
+++ test.py
@@ -3,6 +3,9 @@
 import numpy as np
 import numpy.testing
 
+import scipy as sp
+import scipy.linalg
+
 import unittest
 
 from matlab_matrix_divide import matrix_divide
@@ -18,6 +21,7 @@ class TestMatrixDivision(unittest.TestCase):
 
     def test_mldivide_nonsquare(self):
         a_array = np.asarray([[1, 2, 0], [0, 4, 3]], dtype=np.complex128)
+
         b_array = np.asarray([[8], [18]], dtype=np.complex128)
         c_array = matrix_divide.mldivide(a_array, b_array)
 
